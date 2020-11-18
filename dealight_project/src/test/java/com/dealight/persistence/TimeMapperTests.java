8795107681190cc1a8e13c7fa8433e8f778ad76a
mlicp1994 <hyeonjung@localhost>
2020-11-18 20:20:53 +0900

    mapper -hyeonjung

---
+++ dealight_project/src/test/java/com/dealight/persistence/TimeMapperTests.java
@@ -0,0 +1,35 @@
+package com.dealight.persistence;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.dealight.mapper.TimeMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class TimeMapperTests {
+
+	
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+
+	@Test
+	public void testGetTime2() {
+		log.info(timeMapper.getTime2());
+	}
+}
