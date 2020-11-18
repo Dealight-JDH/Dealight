e649ab2c65321b799a65b54a31a845b930b3556e
mlicp1994 <hyeonjung@localhost>
2020-11-18 16:24:01 +0900

    init commit

--- dealight_project/src/main/java/com/dealight/domain/BStoreVO.java
+++ dealight_project/src/main/java/com/dealight/domain/BStoreVO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BStoreVO {
-	    // 매장번호 
+	    // 매장번호 init 
 	    private Long storeId;
 	    // 사업자회원아이디 
 	    private String buserId;
