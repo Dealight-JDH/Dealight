135e5cc4d58fb6d96fd4cc56886161df549d0ff2
limjongwoo <whddn528@naver.com>
2020-12-17 10:14:52 +0900

    commit --jongwoo

---
+++ dealight_project/src/main/java/com/dealight/domain/RsvdWithStoreDTO.java
@@ -0,0 +1,18 @@
+package com.dealight.domain;
+
+import lombok.Data;
+
+@Data
+public class RsvdWithStoreDTO {
+
+	private Long rsvdId;
+	private Long storeId;
+	private Long htdlId;
+	private String buserId;
+	private int pnum;
+	private String time;
+	private String stusCd;
+	
+	private int acmPnum;
+	
+}
